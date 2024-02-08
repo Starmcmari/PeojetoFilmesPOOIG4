3bc34ff82190da0409b11283a6c96db628281664
Paulo Henrique dos Anjos <127752961+Borkaart@users.noreply.github.com>
2024-02-08 16:33:22 -0400

    Add files via upload

---
+++ src/Filme.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Filme {
+
+    public int id; //torrna private
+
+    public String nome;
+
+    @Override
+    public String toString() {
+        return "Filme{" + "id= " + id +
+                ", nome= '" + nome + '\'' + '}';
+
+
+    }
+
+}
