c26556c99b0cd64420105f7c402e99cdc6f7b8e8
Paulo Henrique dos Anjos <127752961+Borkaart@users.noreply.github.com>
2024-02-08 16:32:14 -0400

    Update FilmesRepository.java

--- src/repository/FilmesRepository.java
+++ src/repository/FilmesRepository.java
@@ -1,13 +1,44 @@
 package repository;
 
-public interface FilmesRepository{
+import Model.Filme;
 
-    public Filme inserir(Filme filme);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public Filme atualizar(Filme filme);
+public class FilmesRepository {
 
-    public void excluir(Filme filme);
+    private static List<Filme> filmes = new ArrayList<>();
 
-    public List<Filme> pesquisarPorNome(String nome);
+    private FilmesRepository(){
+
+    }
+
+    static private int contador = 0;
+
+    public static Filme insere(Filme filme){
+        filme.id = ++contador;
+        filmes.add(filme);
+        return filme;
+    }
+
+    public static Filme alteraNome(int id, String nome){
+        Filme filme = filmes.stream().filter(f -> f.id == id).findFirst().get();
+        filme.nome=nome;
+        return filme;
+    }
+
+    public static void deleta(Filme filme){
+
+    }
+
+    public static List<Filme> obterTodos(){
+        return null;
+    }
+
+    public static List<Filme> pesquisarPorNome(String nome){
+        List<Filme> filmes1= filmes.stream().filter(f -> f.nome.contains(nome)).collect(Collectors.toList());
+        return filmes1;
+    }
 
 }
