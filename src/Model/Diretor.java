42d74fd8bc16c96fd0bd7afec39f13ed0728ca1c
MarianaLeite <mariana.leite@sensedia.com>
2024-02-09 19:54:32 -0300

    Incluindo Ator, Filme e Diretor

--- src/Model/Diretor.java
+++ src/Model/Diretor.java
@@ -1,4 +1,77 @@
 package Model;
 
 public class Diretor {
+    private int id;
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String sexo;
+    private int quantidadeDeFilmes;
+    private int quantidadeDePremios;
+
+    public Diretor(int id, String nome, String sobrenome, int idade, String sexo, int quantidadeDeFilmes, int quantidadeDePremios) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.quantidadeDeFilmes = quantidadeDeFilmes;
+        this.quantidadeDePremios = quantidadeDePremios;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getQuantidadeDeFilmes() {
+        return quantidadeDeFilmes;
+    }
+
+    public void setQuantidadeDeFilmes(int quantidadeDeFilmes) {
+        this.quantidadeDeFilmes = quantidadeDeFilmes;
+    }
+
+    public int getQuantidadeDePremios() {
+        return quantidadeDePremios;
+    }
+
+    public void setQuantidadeDePremios(int quantidadeDePremios) {
+        this.quantidadeDePremios = quantidadeDePremios;
+    }
 }
--- src/Model/Filme.java
+++ src/Model/Filme.java
@@ -1,17 +1,40 @@
 package Model;
 
 public class Filme {
+    private int id;
+    private String nome;
+    private String diretor;
 
-    public int id; //torrna private
+    public Filme(int id, String nome, String diretor) {
+        this.id = id;
+        this.nome = nome;
+        this.diretor = diretor;
+    }
 
-    public String nome;
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
 
     @Override
     public String toString() {
         return "Filme{" + "id= " + id +
                 ", nome= '" + nome + '\'' + '}';
-
-
     }
 
 }
